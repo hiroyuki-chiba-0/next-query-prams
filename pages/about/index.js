4a4c1859c15b477ce5709ec6d2d8c6f93ec17d07
Hiroyuki Chiba <chiba-hiroyuki@cyber-z.co.jp>
2024-04-19 12:27:29 +0900

    add about page

---
+++ pages/about/index.js
@@ -0,0 +1,23 @@
+import styles from '../../styles/Home.module.css'
+
+export const getServerSideProps = async ({ res }) => {
+  res.setHeader('Cache-Control', 'public, max-age=10');
+  return {
+    props: {
+      description: 'This is the about page',
+    },
+  };
+};
+
+export default function About(pageProps) {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          About Page
+        </h1>
+        <p>{pageProps.description}</p>
+      </main>
+    </div>
+  )
+}
